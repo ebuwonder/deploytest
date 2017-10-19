74852e68396d57922f45526fb4139d15a9472bea
ebuwonder <emmayah2@icloud.com>
2017-10-19 17:40:41 -0500

    initial commit

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "Ody"
+    last_name "Amayah"
+    email "ody@ody.com"
+    password "password"
+  end
+end
---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+feature 'authentication' do 
+	before do
+		@user = create(:user)
+	end
+
+	
+end
